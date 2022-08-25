5246eb7e787a1dde1c846e7edbfaa5df097cf4d1
Mohammed AbuShaban <m.aboushaaban@gmail.com>
2022-08-25 09:40:01 +0300

    reverseArray

---
+++ HackerRankSolutions/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HackerRankSolutions;
+
+Console.WriteLine("Hello, World!");
+
+List<int> arr = new List<int>();
+arr.Add(1);
+arr.Add(2);
+arr.Add(3);
+arr.Add(4);
+arr.Add(5);
+
+List<int> res = ReverseArray.reverseArray(arr);
+
+foreach(var i in res)
+{
+    Console.WriteLine(i);
+}
---
+++ HackerRankSolutions/ReverseArray.cs
@@ -0,0 +1,32 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HackerRankSolutions
+{
+    public class ReverseArray
+    {
+        public static List<int> reverseArray(List<int> a)
+        {
+            if (a == null || a.Count < 2) return a;
+            for(int i = 0; i < a.Count / 2; i++)
+            {
+                int temp = a[i];
+                a[i] = a[a.Count - (i + 1)];
+                a[a.Count - (i + 1)] = temp;
+            }
+            return a;
+        }
+
+    }
+}
